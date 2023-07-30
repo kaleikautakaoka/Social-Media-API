a036e28d7a0e3b8bff07c441b8e9e2282e3d44d7
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-07-30 11:40:34 -0600

    index.js api routers exported

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+// require express router and the api routes
+
+
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+//router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
+
+//router.use((req, res)
+router.use((req, res) => {
+    res.status(404).send('<h1>404 Error!</h1>');
+}
+);
+
+module.exports = router;
